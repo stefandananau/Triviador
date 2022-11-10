eda46c27f7b49f077f31eda090567888b21616e0
Ap-Cristian <cristian.apetroaie@student.unitbv.ro>
2022-11-10 16:01:36 +0200

    Added numerical question type to requested json

--- QuestionRequester/QuestionRequester/Source.cpp
+++ QuestionRequester/QuestionRequester/Source.cpp
@@ -2,9 +2,13 @@
 #define URL "https://opentdb.com/api.php?amount=50&type=multiple"
 
 #include <curl/curl.h>
-
+#include <sstream>
 #include <string>
 #include <iostream>
+#include <fstream>
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
@@ -12,12 +16,21 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* use
 	return size * nmemb;
 }
 
+bool isNumber(const std::string& s_toCheck) {
+	for (const char& c : s_toCheck) {
+		if (!std::isdigit(c))
+			return false;
+	}
+	return true;
+}
+
 int main() {
 	CURL* curl;
 	CURLcode res;
-
+	int countQuestions = 0;
+	uint16_t countNumericalQuestions = 0;
 	std::string readBuffer;
-
+	std::string::size_type position = 0;
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
@@ -35,7 +48,76 @@ int main() {
 			std::cout << " curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
 			return -1;
 		}
-		std::cout << readBuffer;
+
+		json finalJsonNumerical;
+		json finalJsonMultipleChoiceQuestions;
+
+		json numericalQuestions;
+		json multipleChoiceQuestions;
+
+		uint16_t indexNumerical = 0;
+		uint16_t indexMultipleChoice = 0;
+		uint16_t curlReqCode;
+
+		while ((indexNumerical < 50) || (indexMultipleChoice < 50)) {
+			readBuffer.clear();
+			res = curl_easy_perform(curl);
+			if (res != CURLE_OK) {
+				std::cout << " curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
+				return -1;
+			}
+
+			json readJson = json::parse(readBuffer);
+			for (int i = 0; i < 50; i++) {
+				if (isNumber(readJson["results"][i]["correct_answer"])) {
+					readJson["results"][i]["type"] = "numerical";
+					++countNumericalQuestions;
+				}
+			}
+
+			for (int i = 0; i < 50; i++) {
+
+				if ((readJson["results"][i]["type"] == "numerical") && (indexNumerical < 50)) {
+					numericalQuestions.push_back(readJson["results"][i]);
+					indexNumerical++;
+				}
+				else if (indexMultipleChoice < 50) {
+					multipleChoiceQuestions.push_back(readJson["results"][i]);
+					indexMultipleChoice++;
+				}
+			}
+			curlReqCode = readJson["response_code"];
+		}
+
+		finalJsonNumerical["response_code"] = curlReqCode;
+		finalJsonNumerical["results"] = numericalQuestions;
+
+		finalJsonMultipleChoiceQuestions["response_code"] = curlReqCode;
+		finalJsonMultipleChoiceQuestions["results"] = multipleChoiceQuestions;
+
+
+		std::cout << "\n_______________________________\n";
+		std::cout << "No. questions: " << finalJsonMultipleChoiceQuestions["results"].size() + finalJsonNumerical["results"].size() << "\n";
+		std::cout << "No. numerical_questions: " << indexNumerical << "\n";
+		std::cout << "No. multiple_choice_questions: " << indexMultipleChoice << "\n";
+		std::cout << "_________________________________\n";
+
+		std::ofstream outNumerical("dataNumerical.json");
+		std::ofstream outMultipleChoice("dataMultipleChoice.json");
+
+		outNumerical << "Number_of_questions: " << finalJsonNumerical["results"].size() << "\n";
+		outMultipleChoice << "Number_of_questions: " << finalJsonMultipleChoiceQuestions["results"].size() << "\n";
+
+		outNumerical << finalJsonNumerical;
+		outMultipleChoice << finalJsonMultipleChoiceQuestions;
+
+		outNumerical.close();
+		outMultipleChoice.close();
+
 	}
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
+
 	return 0;
+
 }
