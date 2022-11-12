dc787bf8a46ef9a0e06c793cf29fac4e2ea860c6
Andrei Apostol <113720385+MuzzleGore@users.noreply.github.com>
2022-11-12 14:36:32 +0200

    Structura pentru memorarea tabelei QuestionMultipleChoice

---
+++ Triviador/Triviador/QuestionMultipleChoiceRecord.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+struct QuestionMultipleChoiceRecord
+{
+	int id;
+	std::string m_question;
+	std::string m_correctAnswer;
+	std::string m_wrongAnswer1;
+	std::string m_wrongAnswer2;
+	std::string m_wrongAnswer3;
+
+};
--- Triviador/Triviador/QuestionNumericRecord.h
+++ Triviador/Triviador/QuestionNumericRecord.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <string>
+
+struct QuestionNumericRecord
+{
+	int id;
+	std::string m_question;
+	std::string m_answer;
+
+
+};
