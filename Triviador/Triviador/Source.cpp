d587dcb87099c376bf22dc636324945837f4e949
Dananau Stefan <95748416+TR45C0X@users.noreply.github.com>
2022-11-19 17:53:34 +0200

    Added CLR project for GUI

--- Triviador/Triviador/Source.cpp
+++ Triviador/Triviador/Source.cpp
@@ -3,33 +3,33 @@
 #include "QuestionNumeric.h"
 #include "Storage.h"
 
-
-int main()
-{	
-
-	std::string category = "sports";
-	std::string type = "sports";
-	std::string difficulty = "sports";
-	std::string question = "sports";
-	std::string correctAnswer = "sports";
-	std::vector<std::string> incorrectAnswers = {"da","nu","poate"};
-	
-
-	QuestionMultipleChoice qmc;
-	QuestionMultipleChoice qmc2( category, type, difficulty, question, correctAnswer,incorrectAnswers);
-
-
-	QuestionNumeric qn;
-	
-	std::cout << qmc<<std::endl<<qmc2;
-	
-	qn.SetQuestion("da");
-
-	Storage db("testDB.sqlite");
-	db.Sync();
-	UserRecord u{ -1,"ceva@gmail.com","1234" };
-	db.AddUser(u);
-	db.PrintUsers();
-	std::cout << qn;
-	return 0;
-}
+//
+//int main()
+//{	
+//
+//	std::string category = "sports";
+//	std::string type = "sports";
+//	std::string difficulty = "sports";
+//	std::string question = "sports";
+//	std::string correctAnswer = "sports";
+//	std::vector<std::string> incorrectAnswers = {"da","nu","poate"};
+//	
+//
+//	QuestionMultipleChoice qmc;
+//	QuestionMultipleChoice qmc2( category, type, difficulty, question, correctAnswer,incorrectAnswers);
+//
+//
+//	QuestionNumeric qn;
+//	
+//	std::cout << qmc<<std::endl<<qmc2;
+//	
+//	qn.SetQuestion("da");
+//
+//	Storage db("testDB.sqlite");
+//	db.Sync();
+//	UserRecord u{ -1,"ceva@gmail.com","1234" };
+//	db.AddUser(u);
+//	db.PrintUsers();
+//	std::cout << qn;
+//	return 0;
+//}
