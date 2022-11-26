0db586f7e01e0cc6d74448cf74117f88c3c0f106
Andrei Apostol <113720385+MuzzleGore@users.noreply.github.com>
2022-11-26 20:57:19 +0200

    gamewindow 3players + board(islands))

---
+++ Triviador/TriviadorGUI/GameWindow3players.cpp
@@ -0,0 +1,13 @@
+#include "GameWindow3players.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TriviadorGUI::GameWindow3players form;
+	Application::Run(% form);
+}
+
