a000d68d66a2dd8702b698224180502fc4732066
Andrei Apostol <113720385+MuzzleGore@users.noreply.github.com>
2022-12-11 19:59:17 +0200

    Revert "4Players Map"
    This reverts commit cd615250e6055bc49560e79eafc30637f1e82bc5.

--- Triviador/TriviadorGUI/main.cpp
+++ Triviador/TriviadorGUI/main.cpp
@@ -1,11 +1,10 @@
 #include "MainWindow.h"
-#include "_4PlayersMap.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    _4PlayersMap w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
