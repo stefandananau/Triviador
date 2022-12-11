4627f059ab84f2953e9991c2559f67c00ad91865
Andrei Apostol <113720385+MuzzleGore@users.noreply.github.com>
2022-12-11 20:20:59 +0200

    4Players Map

--- Triviador/TriviadorGUI/main.cpp
+++ Triviador/TriviadorGUI/main.cpp
@@ -1,10 +1,12 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include "Triviador.h"
+#include "_4PlayersMap.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    _4PlayersMap w;
     w.show();
     return a.exec();
 }
