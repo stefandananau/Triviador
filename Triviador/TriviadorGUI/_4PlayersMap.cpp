d34b005c60fe3a78e9952e34a7270c1df4c82e10
Andrei Apostol <113720385+MuzzleGore@users.noreply.github.com>
2023-01-09 00:39:28 +0200

    icons + 3 players map update

--- Triviador/TriviadorGUI/_4PlayersMap.cpp
+++ Triviador/TriviadorGUI/_4PlayersMap.cpp
@@ -4,6 +4,7 @@ _4PlayersMap::_4PlayersMap(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	setWindowIcon(QIcon(":/new/prefix1/images/ship2.png"));
 }
 
 _4PlayersMap::~_4PlayersMap()
--- Triviador/TriviadorGUI/main.cpp
+++ Triviador/TriviadorGUI/main.cpp
@@ -1,9 +1,10 @@
 #include "MainWindow.h"
+#include "_3PlayersMap.h"
 #include <QtWidgets/QApplication>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    _3PlayersMap w;
     w.show();
     return a.exec();
 }
